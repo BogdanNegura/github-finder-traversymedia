7d236c932fcdc8c74be071a9f7f253d208216f15
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-02 21:25:27 +0100

    fetch api of github users and set an id and client_secret in case if the requests are more then 100times.

---
+++ github.js
@@ -0,0 +1,16 @@
+class GitHub {
+    constructor() {
+        this.client_id = 'c8a6e15cbb8a594fe5e7';
+        this.client_secret = '45fca0f7845cef4a2012d760cb9281964a5ad782';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&this.client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        
+        return{
+            profile
+        }
+    }
+}
