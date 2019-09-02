86a0669bf05cc35b0bbf0e68430b58084e91f216
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-02 21:34:47 +0100

    make http call

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// init GitHub
+const github = new Github;
+
 // Search input
 const searchUser = document.getElementById('searchUser');
 
@@ -6,6 +9,17 @@ searchUser.addEventListener('keyup', (e) => {
     // Get input text
     const userText = e.target.value;
     if(userText !== '') {
-        console.log(userText)
+        // Make http call
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                // Show alert
+
+            } else {
+                // Show profile
+            }
+        })
+    } else {
+        // Clear profile
     }
 });
--- github.js
+++ github.js
@@ -1,4 +1,4 @@
-class GitHub {
+class Github {
     constructor() {
         this.client_id = 'c8a6e15cbb8a594fe5e7';
         this.client_secret = '45fca0f7845cef4a2012d760cb9281964a5ad782';
