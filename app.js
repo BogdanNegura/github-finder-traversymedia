b8049438c78e2e44ee44a9fb701a1c1d503a9826
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-02 22:02:16 +0100

    render profile into DOM in UI when its fetch

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // init GitHub
 const github = new Github;
+// init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -17,6 +19,7 @@ searchUser.addEventListener('keyup', (e) => {
 
             } else {
                 // Show profile
+                ui.showProfile(data.profile);
             }
         })
     } else {
