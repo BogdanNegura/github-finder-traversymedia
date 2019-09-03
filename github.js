9e969d058e6d0232a66341c1c4b19c5485721344
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-03 22:21:44 +0100

    fetch  repos for each user and display latest 5 projects and render them in UI

--- github.js
+++ github.js
@@ -2,15 +2,20 @@ class Github {
     constructor() {
         this.client_id = 'c8a6e15cbb8a594fe5e7';
         this.client_secret = '45fca0f7845cef4a2012d760cb9281964a5ad782';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&this.client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&this.client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
         
         return{
-            profile
+            profile,
+            repos
         }
     }
 }
